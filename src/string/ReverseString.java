bdd537f24db4e79b9d3bc8a2847d4c846d7846ec
Christian Rubiales <christianrubiales@yahoo.com>
2015-10-20 21:21:00 +0800

    ReverseString

---
+++ src/string/ReverseString.java
@@ -0,0 +1,10 @@
+package string;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		String input = "1234567890";
+		System.out.println(new StringBuilder(input).reverse().toString());
+	}
+
+}
